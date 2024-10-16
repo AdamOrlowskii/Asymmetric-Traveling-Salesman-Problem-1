c49eef06c51eec65bb00bfc108060c273e7fb841
AdamOrłowskii <aorlowski644@gmail.com>
2024-10-17 00:11:04 +0200

    macierz

--- pea 1/pea 1.cpp
+++ pea 1/pea 1.cpp
@@ -1,14 +1,50 @@
 ﻿#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 
 
+vector<int> generowanie_wektorow(int liczba_miast){
+	vector<int> miasta;
+	for (int i = 0; i < liczba_miast; i++) {
+		miasta.push_back(i + 1);
+	}
+	return miasta;
+}
 
+vector<vector<int>> losowanie_macierzy(int liczba_miast) {
 
+	vector<vector<int>> macierz_kosztow(liczba_miast, vector<int>(liczba_miast, -1));
+	for (int i = 0; i < liczba_miast; i++) {
+		for (int j = 0; j < liczba_miast; j++) {
+			if (i != j) {
+				macierz_kosztow[i][j] = rand() % 99 + 1;
+			}
+		}
+	}
+	return macierz_kosztow;
+}
 
 int main()
 {
-   
+	int liczba_miast = 4;
+	vector<int> miasta = generowanie_wektorow(liczba_miast);
+
+	for (int miasto : miasta){
+		cout << miasto << " ";
+	}
+	cout << endl;
+
+	vector<vector<int>> macierz_kosztow = losowanie_macierzy(liczba_miast);
+
+	for (int i = 0; i < macierz_kosztow.size(); i++) {
+		for (int j = 0; j < macierz_kosztow[i].size(); j++) {
+			cout << macierz_kosztow[i][j] << " ";
+		}
+		cout << endl;
+	}
+
 }
